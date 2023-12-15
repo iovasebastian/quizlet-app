359d45ee8eb1d83cf4cc8422f7a3f56de01cae7f
iovasebastian <iovasebastian@yahoo.com>
2023-12-15 20:10:22 +0200

    123

--- src/App.js
+++ src/App.js
@@ -7,6 +7,27 @@ import Final from './Final.jsx';
 import './App.css';
 
 function App() {
+  const baseURL = 'https://api.render.com/deploy/srv-clu8bg0cmk4c73875pc0?key=-o7G5B1s-fI';
+const xhr = new XMLHttpRequest();
+
+// Set the CORS headers
+xhr.open('GET', baseURL);
+xhr.setRequestHeader('Access-Control-Allow-Origin', 'https://iovasebastian.github.io');
+xhr.setRequestHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+xhr.setRequestHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+
+// Send the request
+xhr.onload = function() {
+  if (xhr.status === 200) {
+    // Handle the response
+    console.log(xhr.responseText);
+  } else {
+    console.error('Error:', xhr.statusText);
+  }
+};
+
+// Send the request
+xhr.send();
   return (
     <div className="App">
       <HashRouter>
