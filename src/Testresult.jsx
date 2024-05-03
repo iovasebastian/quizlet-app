0dd234e0dc80bdacca28f7a456b404314c103048
iovasebastian <iovasebastian@yahoo.com>
2024-05-03 21:32:12 +0300

    go back

--- src/Testresult.jsx
+++ src/Testresult.jsx
@@ -1,15 +1,20 @@
 import { useState, useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { Navigate, useLocation, useNavigate } from 'react-router-dom';
 import './testresult.css';
 
 const Testresult = () =>{
     const location = useLocation();
+    const navigate = useNavigate();
     const [correctAnswer, setCorrectAnswer] = useState(0);
     const [wrongAnswer, setWrongAnswer] = useState(0);
     useEffect(() => {
         setCorrectAnswer(location.state.correctQuestions);
         setWrongAnswer(location.state.wrongQuestions);
     }, []);
+    const goBack = () =>{
+        navigate('/main');
+    }
+    
     return(
         <div className='backgroundResult'>
             <div className='containerResult'>
@@ -19,6 +24,7 @@ const Testresult = () =>{
                     <p className='wrongAnswer'>Wrong answers : {wrongAnswer}</p>
                 </div>
                 <h1 className='percentResult'>That's {(correctAnswer/10)*100}%</h1>
+                <button onClick = {()=>goBack()} className='buttonBack'>Go back</button>
             </div>
         </div>
     )
