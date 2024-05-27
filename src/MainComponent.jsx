a36cdef2c0102ffb5b09d538c221c92f3c176ea3
iovasebastian <iovasebastian@yahoo.com>
2024-05-27 19:34:16 +0300

    saving error

--- src/MainComponent.jsx
+++ src/MainComponent.jsx
@@ -7,7 +7,7 @@ import downArrow from './downarrow.svg';
 import loadingAnimation from './Rolling-1s-200px.svg';
 
 const baseURL = "https://server-three-taupe.vercel.app/api/items";
-// const baseURL = "http://localhost:3000/api/items";
+//const baseURL = "http://localhost:3000/api/items";
 
 const MainComponent = () => {
   const navigate = useNavigate();
